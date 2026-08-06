200eedf40cd35f53da516b280d1048264fc60d58
Nityansh Pant <24mc3033@rgipt.ac.in>
2026-08-07 01:53:22 +0530

    Update koopmanViT.py

--- koopmanlab/models/koopmanViT.py
+++ koopmanlab/models/koopmanViT.py
@@ -3,7 +3,7 @@ from functools import partial
 from collections import OrderedDict
 from copy import Error, deepcopy
 from re import S
-from numpy.lib.arraypad import pad
+from numpy import pad
 import numpy as np
 import torch
 import torch.nn as nn
