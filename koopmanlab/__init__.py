58133486a340ed4274801bdb7ab607500554e0a7
Nityansh Pant <24mc3033@rgipt.ac.in>
2026-08-11 11:59:13 +0530

    Update __init__.py

--- koopmanlab/__init__.py
+++ koopmanlab/__init__.py
@@ -8,3 +8,4 @@ from .model import koopman, koopman_vit
 from .models import KNO1d,KNO2d
 from .models import encoder_mlp, decoder_mlp, encoder_conv1d, decoder_conv1d, encoder_conv2d, decoder_conv2d
 from .models import ViT
+from . import model_variational
